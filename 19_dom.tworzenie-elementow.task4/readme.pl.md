6285feeacae38e8dda31a6615d18b49748059437
Kamil Chołys <kamilcholys@gmail.com>
2020-01-06 11:19:28 +0000

    init repo

---
+++ 19_dom.tworzenie-elementow.task4/readme.pl.md
@@ -0,0 +1,6 @@
+# Zadanie
+
+
+Na stronie znajduje się guzik.
+ 
+Musiszcie dopisać do niego event w taki sposób, żeby po kliknięciu w niego guzik został usunięty ze strony.
---
+++ 38_es6.fetch.task1/app.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let movieList = document.querySelector('.list');
+    let movieUrl = 'https://swapi.co/api/films/';
+
+
+});
+
+function insertContent( movies, listElemeent) {
+    //tutaj zróbcie pętlę po filmach
+    //wygenerujcie kolejne LI i wstawcie do listy movieList
+}
+
+function loadMovies(url, listElement) {
+    //tutaj wykonaj połączenie za pomocą fetch-a
+}
+
